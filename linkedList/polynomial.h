0a2b20d57dc2a73bfdc6b9226fe7a8243ee48740
Zeyuan On Hadoop <ferrishu3886@gmail.com>
2016-12-11 23:01:58 +0800

    Finish add function

--- linkedList/polynomial.h
+++ linkedList/polynomial.h
@@ -1,7 +1,10 @@
 #ifndef _POLYNOMIALL_H
 #define _POLYNOMIALL_H
 
-/* We assume the nodes sorted by exponents */
+/* We assume the nodes sorted by exponents 
+ * and again, our linked list implementation
+ * uses sentinel node 
+ */
 typedef struct Node *PtrToNode;
 
 struct Node
@@ -14,6 +17,13 @@ struct Node
 typedef PtrToNode Polynomial; 
 
 #endif
+
+/* Create a polynomial from given arrays.
+ * The first input array represents all the coefficients of the polynomial.
+ * The second input array represents all the corresponding exponents.
+ */
+Polynomial initializePolynomial(int coefficient[], int exponent[]);
+
 
 /* 3.6 Write a function to add two polynomials. Do not
  * destroy the input. Use a linked list implementation.
